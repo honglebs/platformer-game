730830286e0bd0dc2069286d23926f0fda75a8b1
Hong Le <hongle622@gmail.com>
2023-07-04 09:09:09 -0700

    added basic settings

--- settings.py
+++ settings.py
@@ -5,3 +5,14 @@ screen_width = 1200
 screen_height = 700
 screen = pg.display.set_mode((screen_width, screen_height))
 clock = pg.time.Clock()
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+    
+    screen.fill('black')
+
+    pg.display.update()
+    clock.tick(60)
