80bc627392fefa9f014695bfdf5aed29b05bffb2
Hong Le <hongle622@gmail.com>
2023-07-15 20:48:52 -0700

    added tile

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame as pg, sys
 from settings import *
+from tiles import Tile
 
 pg.init()
 screen = pg.display.set_mode((screen_width, screen_height))
 clock = pg.time.Clock()
+test_tile = pg.sprite.Group(Tile((100, 100), 200))
 
 while True:
     for event in pg.event.get():
@@ -12,6 +14,7 @@ while True:
             sys.exit()
     
     screen.fill('black')
+    test_tile.draw(screen)
 
     pg.display.update()
     clock.tick(60)
--- tile.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, size):
-        super().__init__()
-        self.image
-        self.rect
